447af59327e2f955b6aa918cd9bd8d8119a4316d
Tyler <tylerbalaskovitz@yahoo.com>
2023-12-31 12:06:51 -0500

    Adding the helloAgain.c to the commits for coding credit/history

---
+++ helloAgain.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+	if (puts("Hello, world!") == EOF){
+	return EXIT_FAILURE;
+	//code here never executes
+	}
+return EXIT_SUCCESS;
+//Code here also never executes
+}
+
