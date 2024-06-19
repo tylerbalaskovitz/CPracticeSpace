8c4a8a949c701e658588bd2892cc00e09524001b
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-06-19 17:30:14 -0400

    Using struct and typedef to create some monsters for the game

---
+++ textrpg/battlesystem.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+extern int combat(char monster, int hp, int def, int att, int mag){
+
+}
---
+++ textrpg/monsters.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct{
+	char name[256];
+	int hp;
+	int def;
+	int att;
+	int mag;
+} monster;
+
+
+typedef struct{
+	char name[256];
+	int hp;
+	int def;
+	int att;
+	int mag;
+} player; 
