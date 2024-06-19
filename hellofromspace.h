6ff2dae1cbec8f24c05c6cf141b7bb83bcf2fbd8
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-06-18 22:26:48 -0400

    Learning how to include other headers within a file and creating functions that can be used elsewhere

---
+++ hellofromspace.h
@@ -0,0 +1,9 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+#include <stdio.h>
+
+void HelloAgain(){
+	printf("Hello, from another world");
+};
+
+#endif // MYHEADER_H
--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "./hellofromspace.h"
 int main(){
 	//printf() displays the string inside the quotation
 	printf("Hello, world!");
+	HelloAgain();
 	return 0;
 }
