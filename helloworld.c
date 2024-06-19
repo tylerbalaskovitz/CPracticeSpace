e3af8aef40c0fd257c4289da8174a9a71e46f3e4
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-06-19 17:17:14 -0400

    Adding a bit to learn with debugging and using gdb

--- helloworld.c
+++ helloworld.c
@@ -6,7 +6,10 @@ int main(){
 	//printf() displays the string inside the quotation
 	printf("Hello, world!\n");
 	HelloAgain();
-	printf("Jenny, I got your number%d\n", phonenumber);
-	printf("My favorite food is%s\n", favoritefood);
+	printf("Jenny, I got your number %d\n", phonenumber);
+	printf("My favorite food is %s\n", favoritefood);
+	printf("What's your favorite food?");
+	scanf("%s", &favoritefood);
+	printf("I'm glad that you like to eat %s", favoritefood);
 	return 0;
 }
