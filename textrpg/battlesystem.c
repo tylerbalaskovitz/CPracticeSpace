a5ebd92c9fb69a0dbca3943082e9bddd28d5e6ff
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-06-20 15:24:21 -0400

    Was able to start passing variables into the function to act as values for the RPG

--- textrpg/battlesystem.c
+++ textrpg/battlesystem.c
@@ -3,7 +3,8 @@
 int monsterAttack(char monster[50], int hp, int def, int att, int mag);
 int playerAttack(char player[50], int hp, int def, int att, int mag);
 int combat(char monster, int hp, int def, int att, int mag);
-
+char monName[50] = "Wolf";
+int monHp, monDef, monAtt, monMag;
 bool innitiative = true;
 bool ingame = true;
 int battleCondition = 0;
@@ -11,7 +12,7 @@ int battleCondition = 0;
  int main() {
 	 while (ingame){
 	 battleCondition = playerAttack("Test", 0, 0,0,0);
-	 battleCondition = monsterAttack("Test2", 0,0,0,0);		
+	 battleCondition = monsterAttack(monName, monHp,monDef,monAtt,monMag);		
 	
 	if (battleCondition = 1){
 	ingame = false;
@@ -27,6 +28,8 @@ return 0;
 
 int playerAttack(char player[50], int hp, int def, int att, int mag){
 printf("Inside player attack, %s\n", player);	
+int playerMove=0;
+scanf("%d", &playerMove);
 
 return 0;
 }
