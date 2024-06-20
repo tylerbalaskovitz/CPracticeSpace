973ae31c54a3aaf0c9922453a1c16717ff46e6a9
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-06-20 13:53:49 -0400

    Adding the beginning of the combat system

--- textrpg/battlesystem.c
+++ textrpg/battlesystem.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 extern int combat(char monster, int hp, int def, int att, int mag){
 
+
 }
+bool innitiative = true;
+bool ingame = true;
+
+ int main() {
+	 while (ingame){
+	int battleCondition = 0;
+	battleCondition = playerAttack();
+	battleCondition = monsterAttack();		
+	
+	if (battleCondition = 1){
+	ingame = false;
+	}
+
+	 }
+
+
+
+return 0;
+ }
+
+int playerAttack(char monster, int hp, int def, int att, int mag){
+
+
+return 0;
+}
+int monsterAttack(char monster, int hp, int def, int att, int mag){
+	
+
+
+return 0;
+}
+
