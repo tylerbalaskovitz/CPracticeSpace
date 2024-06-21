53b7ea3474ecf15bc395a8a6b4451ce4d9177cb1
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-06-20 21:47:16 -0400

    Adding more to the players battle screen

--- textrpg/battlesystem.c
+++ textrpg/battlesystem.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
 int monsterAttack(char monster[50], int hp, int def, int att, int mag);
 int playerAttack(char player[50], int hp, int def, int att, int mag);
 int combat(char monster, int hp, int def, int att, int mag);
 
 char playName[50] = "Ralphie";
-int playHp, playDef, playAtt, playMag;
-
+int playHp = 30, playDef = 5, playAtt = 20, playMag = 25, playTempDef = 0;
 char monName[50] = "Wolf";
 int monHp, monDef, monAtt, monMag;
 bool innitiative = true;
-bool ingame = true;
+bool inBattle = true;
 int battleCondition = 0;
+bool victory = false;
 
  int main() {
-	 while (ingame){
-	 battleCondition = playerAttack("Test", 0, 0,0,0);
+	 while (inBattle){
+	 battleCondition = playerAttack(playName, playHp, playDef, playAtt, playMag);
 	 battleCondition = monsterAttack(monName, monHp,monDef,monAtt,monMag);		
 	
 	if (battleCondition = 1){
-	ingame = false;
+	inBattle = false;
 	}
 
 	 }
@@ -33,11 +35,20 @@ return 0;
 int playerAttack(char player[50], int hp, int def, int att, int mag){
 printf("Enemy name:  %s\n", monName);	
 printf("Enemy HP: %d Enemy Defense: %d, Enemy Attack: %d, Enemy Magic: %d\n", monHp, monDef, monAtt, monMag); 
+printf("=============================================================================================\n");
 printf("Player name:  %s\n", player);	
-printf("Player HP: %d Player Defense: %d, Player Attack: %d, Player Magic: %d\n", hp, def, att, mag); 
-int playerMove=0;
-scanf("%d", &playerMove);
-
+printf("Player HP: %d Player Defense: %d, Player Attack: %d, Player Magic: %d\n", hp, def, att, mag);
+printf("1: Attack\n 2: Defend\n 3: Magic Attack\n 4:Pass");
+int playerAttack=0;
+scanf("%d", &playerAttack);
+switch(playerAttack){
+	case 1: monHp= monHp - att+monDef; break;
+	case 2: playTempDef = playTempDef+2  ; break;
+	case 3: monHp= monHp - mag+monDef; break;
+	case 4: printf("You pass this round. Wow neat!"); break;
+	default:printf("You pass this round. Wow neat!"); break;
+ 
+}
 return 0;
 }
 int monsterAttack(char monster[50], int hp, int def, int att, int mag){
@@ -45,5 +56,9 @@ printf("Enemy name:  %s\n", monster);
 printf("Enemy HP: %d Enemy Defense: %d, Enemy Attack: %d, Enemy Magic: %d\n", hp, def, att, mag); 
 
 
-return 1;
+return 0;
+}
+
+void magicAttack(int target, int damage){
+
 }
